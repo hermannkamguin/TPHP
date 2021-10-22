3cebd1e807aee04a7120acde2f439bf8f373da1b
KAMGUIN HERMANN <hermann@MacBook-Pro-de-KAMGUIN.local>
2021-10-22 12:31:27 +0200

    change

--- Connexion.php
+++ Connexion.php
@@ -5,7 +5,7 @@ class Connexion
     public static $bdd;
 
     public static function initConnexion(){
-        self::$bdd = new PDO('mysql:host=localhost;dbname=progweb;charset=utf8', 'id15599947_progweb', '6h<}Y%Y]khVt6nO=');
+        self::$bdd = new PDO('mysql:host=localhost;dbname=id15599947_progweb;charset=utf8', 'id15599947_progweb1', '6h<}Y%Y]khVt6nO=');
     }
 
 }
